0803ed02358a0a0d06beeea6049b6ea370bac3d0
xinyugao233 <xinyugao233@gmail.com>
2025-01-24 19:16:41 -0600

    验证码

--- onlineshopproject/onlineshopproject/settings.py
+++ onlineshopproject/onlineshopproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users',
+    'apps.verifications',
     'corsheaders',
 ]
 
@@ -148,6 +149,13 @@ CACHES = {
     "session": { # session
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "code": { # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
--- onlineshopproject/onlineshopproject/urls.py
+++ onlineshopproject/onlineshopproject/urls.py
@@ -36,4 +36,5 @@ register_converter(UsernameConverter, 'username')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),
+    path('', include('apps.verifications.urls')),
 ]
